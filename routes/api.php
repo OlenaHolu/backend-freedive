bcf3ce2965208d88da1eb1f73a1ecedb0ab75c8f
Olena.Holub <olehol@alu.edu.gva.ua>
2025-03-24 12:58:20 +0100

    add routes dive

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DiveController;
 
 //Protected routes
 Route::middleware(['firebase'])->group(function () {

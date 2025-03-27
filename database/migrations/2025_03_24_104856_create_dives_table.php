3ddb8f7e11e065d359dc3c1e9c9ed337ab6f316c
Olena.Holub <oleholu@gmail.com>
2025-03-27 12:51:52 +0100

    change DB

--- database/migrations/2025_03_24_104856_create_dives_table.php
+++ database/migrations/2025_03_24_104856_create_dives_table.php
@@ -10,44 +10,50 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up()
-{
-    Schema::create('dives', function (Blueprint $table) {
-        $table->id();
-        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+    {
+        Schema::create('dives', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
-        $table->timestamp('StartTime');
-        $table->integer('Duration');
-        $table->decimal('MaxDepth', 8, 2);
-        
-        $table->smallInteger('SampleInterval')->nullable();
-        $table->smallInteger('Mode')->default(3);
-        $table->text('Source')->nullable();
-        $table->text('Note')->nullable();
-        $table->smallInteger('StartTemperature')->nullable();
-        $table->smallInteger('BottomTemperature')->nullable();
-        $table->smallInteger('EndTemperature')->nullable();
-        $table->smallInteger('AltitudeMode')->nullable();
-        $table->smallInteger('PersonalMode')->nullable();
-        $table->integer('DiveNumberInSerie')->nullable();
-        $table->integer('SurfaceTime')->nullable();
-        $table->integer('SurfacePressure')->nullable();
-        $table->decimal('PreviousMaxDepth', 8, 2)->nullable();
-        $table->integer('DiveTime')->nullable();
-        $table->boolean('Deleted')->default(false);
-        $table->double('Weight')->nullable();
-        $table->integer('Weather')->nullable();
-        $table->integer('Visibility')->nullable();
-        $table->double('AvgDepth')->nullable();
-        $table->text('Software')->nullable();
-        $table->string('SerialNumber')->default('');
-        $table->integer('TimeFromReset')->nullable();
-        $table->double('Battery')->nullable();
-        $table->double('LastDecoStopDepth')->default(3.0);
-        $table->smallInteger('AscentMode')->default(0);
+            $table->timestamp('StartTime');
+            $table->integer('Duration');
+            $table->decimal('MaxDepth', 8, 2);
+            $table->decimal('AvgDepth', 8, 2)->nullable();
 
-        $table->timestamps();
-    });
-}
+            $table->smallInteger('SampleInterval')->nullable();
+            $table->smallInteger('Mode')->default(3);
+            $table->text('Source')->nullable();
+            $table->text('Note')->nullable();
+
+            $table->decimal('StartTemperature', 5, 2)->nullable();
+            $table->decimal('BottomTemperature', 5, 2)->nullable();
+            $table->decimal('EndTemperature', 5, 2)->nullable();
+
+            $table->smallInteger('AltitudeMode')->nullable();
+            $table->smallInteger('PersonalMode')->nullable();
+            $table->integer('DiveNumberInSerie')->nullable();
+            $table->integer('SurfaceTime')->nullable();
+            $table->integer('SurfacePressure')->nullable();
+
+            $table->decimal('PreviousMaxDepth', 8, 2)->nullable();
+            $table->integer('DiveTime')->nullable();
+            $table->boolean('Deleted')->default(false);
+
+            $table->decimal('Weight', 5, 2)->nullable();
+            $table->integer('Weather')->nullable();
+            $table->integer('Visibility')->nullable();
+
+            $table->text('Software')->nullable();
+            $table->string('SerialNumber')->default('');
+            $table->integer('TimeFromReset')->nullable();
+            $table->decimal('Battery', 4, 2)->nullable();
+
+            $table->decimal('LastDecoStopDepth', 5, 2)->nullable();
+            $table->smallInteger('AscentMode')->default(0);
+
+            $table->timestamps();
+        });
+    }
 
 
     /**

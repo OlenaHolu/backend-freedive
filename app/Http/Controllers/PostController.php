fa573709d5b373b6772187d163e1ee7f8e8f7b7f
Olena.Holub <oleholu@gmail.com>
2025-04-15 11:29:22 +0200

    add debug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Facades\Http;
 
 class PostController extends Controller
